7bf42278a27894cb82957f1fcca8548c1428335e
Ibrahim-Kaldesh <ebrahim195011@feng.bu.edu.eg>
2023-08-25 20:26:06 +0300

    Assignment.js & Assignment.css has been added

---
+++ src/Assignment.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Assignment.css";
+
+const Assignment = () => {
+  return (
+    <div>
+      <header>
+        <h1>مراقب مبني</h1>
+        {/* <div className="header-icon"></div> */}
+      </header>
+      <div className="full-width-div">
+        اختر ايام المراقبة والملاحظة المناسبة لك لتكليف ملاحظة الامتحانات لعام
+        2024
+      </div>
+      <div className="three-divs">
+        <div className="one-third">
+          <div>عدد الايام المطلوبة</div>
+          <div className="days">6</div>
+        </div>
+        <div className="one-third">
+          <div>عدد الايام المتاحة</div>
+          <div className="days">3</div>
+        </div>
+        <div className="one-third">
+          <div>الزمن المتبقي</div>
+          <div className="timer">05:30:04</div>
+        </div>
+      </div>
+      <div className="circle-icon">
+        <div className="arrow"></div>
+      </div>
+
+      <div className="warning">
+        <div className="warning">
+          في حالة عدم اختيار عدد الايام المطلوبة.سيتم توزيع ايام التكليف
+          المتبقية الخاصة بك بشكل عشوائي
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Assignment;
